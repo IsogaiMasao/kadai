76a6f54889b150460aad6c80ea35452b044348e9
masao <kyara_meruru@yahoo.co.jp>
2019-03-05 02:15:42 +0900

    2週目Laravel15

---
+++ 2ndLaravel15/ProfileController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Profile;
+
+class ProfileController extends Controller
+{
+  public function add()
+  {
+    return view('admin.profile.create');
+  }
+
+  public function create(Request $request)
+  {
+    $this->validate($request, Profile::$rules);
+
+    $profile = new Profile;
+    $form = $request->all();
+
+    if(isset($form['image'])){
+      $path = $request->file('image')->store('public/image');
+      $profile->image_path = basename($path);
+    }else{
+      $profile->image_path = null;
+    }
+
+    unset($form['_token']);
+    unset($form['image']);
+
+    $profile->fill($form);
+    $profile->save();
+
+    return redirect('admin/profile/create');
+  }
+
+  public function edit()
+  {
+    return view('admin.profile.edit');
+  }
+
+  public function update()
+  {
+    return redirect('admin/profile/edit');
+  }
+
+
+}
